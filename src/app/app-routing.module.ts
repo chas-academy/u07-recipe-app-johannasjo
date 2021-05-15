0ec9fdff9db7528a4a99dec4971ae9dc7e370b17
johannasjo <johanna.sjoestrand@gmail.com>
2021-05-15 17:05:57 +0200

    add nav links and looks for login view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,31 +3,46 @@ import { Routes, RouterModule } from '@angular/router';
 import { RecipeLandingComponent } from './recipes/recipe-landing/recipe-landing.component';
 import { RecipeUserListComponent } from './recipes/recipe-favorite-list/recipe-favorite-list.component';
 import { RecipeViewComponent } from './recipes/recipe-view/recipe-view.component';
+import { RecipeListsComponent } from './recipes/recipe-lists/recipe-lists.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: 'recipes',
-    component: RecipeLandingComponent,
+    component: RecipeLandingComponent
   },
   {
     path: 'recipes/:id',
-    component: RecipeViewComponent,
+    component: RecipeViewComponent
   },
   {
     path: 'favorites',
-    component: RecipeUserListComponent,
+    component: RecipeUserListComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'favorites/all',
+    component: RecipeListsComponent
   },
   // default to recipes
   {
     path: '',
     redirectTo: '/recipes',
-    pathMatch: 'full',
-  },
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
   declarations: [],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
