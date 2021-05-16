1db4129c4c42dcc34297d16d9b5c983eb855e0d8
johannasjo <johanna.sjoestrand@gmail.com>
2021-05-16 14:28:35 +0200

    add rerouting on login and register

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
 import { AuthData } from './auth-data.model';
@@ -17,7 +18,7 @@ export class AuthService {
   private authStatusListener = new Subject<boolean>();
   baseUrl = 'https://git.heroku.com/josjo-recipe-backend.git';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getToken() {
     return this.token;
@@ -49,7 +50,9 @@ export class AuthService {
         if (token) {
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
-        }
+          this.router.navigate(['/']);
+        } // if no account exists, reroute to register
+        // if email is taken, display that error to user
       });
   }
 
@@ -57,5 +60,6 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 }
